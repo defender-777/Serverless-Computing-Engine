90c67a4f0b4abe6a639db91c0f414e5423448687
Gagan R <gagan.r.2005@gmail.com>
2026-03-04 17:13:51 +0530

    Server and Database Initialization

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// connect database
+connectDB();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ status: "Server running" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
